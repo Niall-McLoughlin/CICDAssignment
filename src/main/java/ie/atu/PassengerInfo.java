c2c84f17e63c7b0c02e7208e7ef20600282b4480
Niall McLoughlin <G00362680@atu.ie>
2022-11-05 23:05:16 +0000

    ended up having to hardcode test data

--- src/main/java/ie/atu/PassengerInfo.java
+++ src/main/java/ie/atu/PassengerInfo.java
@@ -2,12 +2,30 @@ package ie.atu;
 
 public class PassengerInfo {
     private int countValue;
+    private String titleValue;
+    private String nameValue;
+    private String IDValue;
+    private String phoneValue;
+    private int ageValue;
+    int titleOK =0;
+    int nameOK =0;
+    int IDOK =0;
+    int phoneOK =0;
+    int ageOK =0;
+
+
+
     public PassengerInfo() {
-        countValue = 0;
+        titleValue = "Mr";
+        nameValue = "John Doe";
+        IDValue = "1045";
+        phoneValue = "0861234567";
+        ageValue = 17;
     }
-    public PassengerInfo(int startingValue) {
-        if (startingValue > 5 ) {
-            countValue = startingValue;
+
+    public PassengerInfo(int tartingValue) {
+        if (tartingValue > 5 ) {
+            countValue = tartingValue;
         }
         else
         {
@@ -15,8 +33,36 @@ public class PassengerInfo {
         }
     }
 
-    public int increment() {
-countValue++;
-return countValue;
+    public int name() {
+        if (nameValue.length() >= 3) {
+            nameOK++;
+        }
+        return nameOK;
+    }
+    public int id() {
+        if (IDValue.length() >= 10) {
+            IDOK++;
+        }
+        return IDOK;
+    }
+    public int phone() {
+        if (phoneValue.length() >= 7) {
+            phoneOK++;
+        }
+        return phoneOK;
+    }
+
+    public int title() {
+        if (titleValue == "Mrs" || titleValue == "Mr" || titleValue == "Ms") {
+            titleOK++;
+        }
+        return titleOK++;
+    }
+
+    public int age() {
+        if (ageValue >= 16) {
+            ageOK++;
+        }
+    return ageOK;
     }
 }
--- src/test/java/ie/atu/PassengerInfoTest.java
+++ src/test/java/ie/atu/PassengerInfoTest.java
@@ -7,24 +7,69 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PassengerInfoTest {
-    PassengerInfo myCount;
 
+    PassengerInfo myAge;
+    PassengerInfo myTitle;
+    PassengerInfo myName;
+    PassengerInfo myID;
+    PassengerInfo myPhone;
     @BeforeEach
     void setUp() {
-        myCount = new PassengerInfo();
-    }
-    @Test
-    void testIncrement()
-    {
-    assertEquals(1,myCount.increment());
+
+        myTitle = new PassengerInfo();
+        myName = new PassengerInfo();
+        myID = new PassengerInfo();
+        myPhone = new PassengerInfo();
+        myAge = new PassengerInfo();
     }
 
+
     @Test
-    void testConstructor()
+    void testTitle()
     {
-       Exception exMessage =  assertThrows(IllegalArgumentException.class, ()-> { new PassengerInfo(4);});
-       assertEquals("This is not a valid number", exMessage.getMessage());
+        assertEquals(1, myTitle.title());
     }
+    @Test
+    void testName()
+    {
+        assertEquals(1,myName.name());
+    }
+    @Test
+    void testID()
+    {
+        assertEquals(1,myID.id());
+    }
+    @Test
+    void testPhone()
+    {
+        assertEquals(1,myPhone.phone());
+    }
+    @Test
+    void testAge()
+    {
+        assertEquals(1,myAge.age());
+    }
+
+
+
+
+
+
+
+
+
+
+
+    @Test
+    void testConstructor() {
+        Exception exMessage = assertThrows(IllegalArgumentException.class, () -> {
+            new PassengerInfo(4);
+        });
+        assertEquals("This is not a valid number", exMessage.getMessage());
+    }
+
+
+
 
     @AfterEach
     void tearDown() {
